e8efb753853ec43ce2753d268947f72aee409d39
m1ner79 <michal.d.gornicki@gmail.com>
2020-05-28 23:02:02 +0100

    testing pull_request

--- index.js
+++ index.js
@@ -46,6 +46,6 @@ switch (process.env.GITHUB_EVENT_NAME) {
 const paramPost = {status: tweetingStatus};
 
 postTweets(client, paramPost)
-  .then((resp)=>{core.info(`Your tweet is posted here: https://twitter.com/${resp.user.screen_name}/status/${resp.id}`)})
+  .then((resp)=>{core.info(`Your tweet is posted here: https://twitter.com/${resp.user.screen_name}/status/${resp.id}`); console.log(resp)})
   .catch((err)=>{throw new Error(err.message)});
 
