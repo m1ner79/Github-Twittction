c264db17c9a1a2d373c50028fcc4dcbcc86f77ff
m1ner79 <michal.d.gornicki@gmail.com>
2020-05-30 08:39:38 +0100

    refactor function getDefaultMessage

--- dist/index.js
+++ dist/index.js
@@ -80,12 +80,14 @@ const Twitter = __webpack_require__(851);
 const core = __webpack_require__(470);
 const { readFileSync } = __webpack_require__(747);
 const { postTweets } = __webpack_require__(0);
-//const { defaultMessages } = require("./src/utils");
+const { getDefaultMessage } = __webpack_require__(543);
 
 const consumer_key = core.getInput('twitter_consumer_key', { required: true }) || process.env.TWITTER_CONSUMER_KEY;
 const consumer_secret = core.getInput('twitter_consumer_secret', { required: true }) || process.env.TWITTER_CONSUMER_SECRET;
 const access_token_key = core.getInput('twitter_access_token_key', { required: true }) || process.env.TWITTER_ACCESS_TOKEN_KEY;
 const access_token_secret = core.getInput('twitter_access_token_secret', { required: true }) || process.env.TWITTER_ACCESS_TOKEN_SECRET;
+
+const message = core.getInput('twitter_status');
 
 const client = new Twitter({
   consumer_key,
@@ -98,28 +100,7 @@ const payload = JSON.parse(
   readFileSync(process.env.GITHUB_EVENT_PATH, "utf8")
 );
 
-const message = core.getInput('twitter_status');
-
-let tweetingStatus;
-//defaultMessages
-switch (process.env.GITHUB_EVENT_NAME) {
-  case "push":
-    tweetingStatus = message || `${payload.pusher.name} just created a commit to ${payload.repository.full_name}. More details are available here: ${payload.commits[0].url}`;
-    break;
-  case "pull_request":
-    tweetingStatus = message || `${payload.pull_request.head.repo.full_name} just created a pull request: ${payload.pull_request.title}. More info is available here: ${payload.pull_request.html_url}`;
-    break;
-  case "release":
-    tweetingStatus = message || `A new release ${payload.release.tag_name} in ${payload.repository.full_name}. More details are available here ${payload.release.html_url}`;
-    break;
-  default:
-    if (message) {
-      tweetingStatus = message;
-    } else {
-      throw new Error(`${process.env.GITHUB_EVENT_NAME} is not supported with default message. Provide custom message using tweeter_status input parameter.`);
-    }
-    break;
-}
+const tweetingStatus  = message || getDefaultMessage(process.env.GITHUB_EVENT_NAME, payload);
 
 const paramPost = {status: tweetingStatus};
 
@@ -2107,6 +2088,33 @@ function getState(name) {
 }
 exports.getState = getState;
 //# sourceMappingURL=core.js.map
+
+/***/ }),
+
+/***/ 543:
+/***/ (function(module) {
+
+
+function getDefaultMessage(eventName, payload){
+  let tweetingStatus;
+  //defaultMessages
+  switch (eventName) {
+    case "push":
+      tweetingStatus = `${payload.pusher.name} just created a commit to ${payload.repository.full_name}. More details are available here: ${payload.commits[0].url}`;
+      break;
+    case "pull_request":
+      tweetingStatus = `${payload.pull_request.head.repo.full_name} just created a pull request: ${payload.pull_request.title}. More info is available here: ${payload.pull_request.html_url}`;
+      break;
+    case "release":
+      tweetingStatus = `A new release ${payload.release.tag_name} in ${payload.repository.full_name}. More details are available here ${payload.release.html_url}`;
+      break;
+    default:
+        throw new Error(`${eventName} is not supported with default message. Provide custom message using tweeter_status input parameter.`);
+      break;
+  }
+  return tweetingStatus;
+}
+  module.exports.getDefaultMessage = getDefaultMessage;
 
 /***/ }),
 
--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const Twitter = require('twitter-lite');
 const core = require('@actions/core');
 const { readFileSync } = require("fs");
 const { postTweets } = require("./src/tweety");
-//const { defaultMessages } = require("./src/utils");
+const { getDefaultMessage } = require("./src/utils");
 
 const consumer_key = core.getInput('twitter_consumer_key', { required: true }) || process.env.TWITTER_CONSUMER_KEY;
 const consumer_secret = core.getInput('twitter_consumer_secret', { required: true }) || process.env.TWITTER_CONSUMER_SECRET;
 const access_token_key = core.getInput('twitter_access_token_key', { required: true }) || process.env.TWITTER_ACCESS_TOKEN_KEY;
 const access_token_secret = core.getInput('twitter_access_token_secret', { required: true }) || process.env.TWITTER_ACCESS_TOKEN_SECRET;
+
+const message = core.getInput('twitter_status');
 
 const client = new Twitter({
   consumer_key,
@@ -20,28 +22,7 @@ const payload = JSON.parse(
   readFileSync(process.env.GITHUB_EVENT_PATH, "utf8")
 );
 
-const message = core.getInput('twitter_status');
-
-let tweetingStatus;
-//defaultMessages
-switch (process.env.GITHUB_EVENT_NAME) {
-  case "push":
-    tweetingStatus = message || `${payload.pusher.name} just created a commit to ${payload.repository.full_name}. More details are available here: ${payload.commits[0].url}`;
-    break;
-  case "pull_request":
-    tweetingStatus = message || `${payload.pull_request.head.repo.full_name} just created a pull request: ${payload.pull_request.title}. More info is available here: ${payload.pull_request.html_url}`;
-    break;
-  case "release":
-    tweetingStatus = message || `A new release ${payload.release.tag_name} in ${payload.repository.full_name}. More details are available here ${payload.release.html_url}`;
-    break;
-  default:
-    if (message) {
-      tweetingStatus = message;
-    } else {
-      throw new Error(`${process.env.GITHUB_EVENT_NAME} is not supported with default message. Provide custom message using tweeter_status input parameter.`);
-    }
-    break;
-}
+const tweetingStatus  = message || getDefaultMessage(process.env.GITHUB_EVENT_NAME, payload);
 
 const paramPost = {status: tweetingStatus};
 
