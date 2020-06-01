5651735a23a28ed2d679934b2b6a3cedf46463c2
m1ner79 <michal.d.gornicki@gmail.com>
2020-06-01 09:10:24 +0100

    add test

---
+++ test/utils.test.js
@@ -0,0 +1,11 @@
+
+const { getDefaultMessage } = require('../src/utils');
+
+test1('should return defaultMessage', () => {
+  //expect(paramParser('baseHref=/my-repo-name/ sidebarOrganization=byTags')).toEqual({baseHref: '/my-repo-name/', sidebarOrganization: 'byTags'});
+});
+
+
+test2('should return an error if event not supported', () => {
+ // expect(paramParser('baseHref=/my-repo-name/ sidebarOrganization=byTags')).toEqual({baseHref: '/my-repo-name/', sidebarOrganization: 'byTags'});
+});
