2ce3e1f5f3b5e4db77e3cef302d0628a09ee6a38
m1ner79 <michal.d.gornicki@gmail.com>
2020-06-02 09:11:19 +0100

    update tests

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,11 +1,35 @@
 
 const { getDefaultMessage } = require('../src/utils');
 
-test1('should return defaultMessage', () => {
-  //expect(paramParser('baseHref=/my-repo-name/ sidebarOrganization=byTags')).toEqual({baseHref: '/my-repo-name/', sidebarOrganization: 'byTags'});
+test('should return defaultMessage', () => {
+  const payload = {
+    commits: [{
+      url: 'test url'
+    }
+    ],
+    repository: {
+      full_name: 'Codertocat/Hello-World' 
+    },
+    pusher: {
+      name: 'Codertocat'
+    }
+  };
+  const defaultMessage = getDefaultMessage('push', payload);
+  expect(defaultMessage).toEqual('Codertocat just created a commit to Codertocat/Hello-World. More details are available here: test url');
 });
 
-
-test2('should return an error if event not supported', () => {
- // expect(paramParser('baseHref=/my-repo-name/ sidebarOrganization=byTags')).toEqual({baseHref: '/my-repo-name/', sidebarOrganization: 'byTags'});
+test('should return an error if event not supported', () => {
+  const payload = {
+    commits: [{
+      url: 'test url'
+    }
+    ],
+    repository: {
+      full_name: 'Codertocat/Hello-World' 
+    },
+    pusher: {
+      name: 'Codertocat'
+    }
+  };
+  expect(() => getDefaultMessage('test', payload)).toThrow('test is not supported with default message. Provide custom message using tweeter_status input parameter.');
 });
